eb1fac5836f9c4a7e1345b6505d84973f836a87b
Gopal Penny <gopalpenny@gmail.com>
2020-12-12 12:13:42 -0500

    updated hh_grid_example with id

--- R/gw-data.R
+++ R/gw-data.R
@@ -4,5 +4,4 @@
 #' Gridded household data
 #'
 #' Example data to get started containing a 4x4 grid of households
-#' @export
 "hh_grid_example"
--- data-raw/hh_grid_example.R
+++ data-raw/hh_grid_example.R
@@ -1,7 +1,12 @@
 ## code to prepare `hh_grid_example` dataset goes here
 
+
 density <- units::set_units(0.25,"1/acre")
 area <- units::set_units(64,"acre")
 hh_grid_example <- nitratesgame::get_hh_grid(density,area)
+hh_grid_example$z1 <- units::set_units(10,"ft")
+hh_grid_example$z2 <- units::set_units(18,"ft")
+hh_grid_example$rs <- units::set_units(10,"ft")
+hh_grid_example$id <- 1:nrow(hh_grid_example)
 
 usethis::use_data(hh_grid_example, overwrite = TRUE)
